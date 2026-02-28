b5097d1174fa7505bacbc71b2a3cf490168b615c
Akshat Kala <itzakshat706@gmail.com>
2026-03-01 00:30:28 +0530

    Updated Attendance

--- server/routes/scan.js
+++ server/routes/scan.js
@@ -1,33 +1,49 @@
 const express = require("express");
 const scan = express();
 const User = require("../models/user");
-const updatedUser = require('../models/updatedStudentDetails')
+const updatedUser = require("../models/updatedStudentDetails");
+const eventAttendance = require("../models/EventAttendance");
+
 
 scan.get("/user/:id", async (req, res) => {
   const { id } = req.params;
 
-  if (id) {
-    try {
-      const userData = await User.findById(id);
-      const updatedUserData = await updatedUser.findOne({connectionId: id})
+  if (!id) {
+    return res.status(400).json({ err: "User does not exist!" });
+  }
 
-      return res.json({
-        name:  userData.name,
-        designation:  userData.role,
-        email:  userData.email,
-        phone:  userData.phone,
-        branch:  updatedUserData.branch,
-        year:  updatedUserData.year,
-        roll:  updatedUserData.rollNumber,
-        dob:  updatedUserData.dob,
-        father: updatedUserData.fatherName,
-        photo: userData.dpurl,
-      });
-    } catch {
-        return res.status(400).json({err: 'Something went wrong!'})
+  try {
+    const userData = await User.findById(id);
+    const updatedUserData = await updatedUser.findOne({ connectionId: id });
+
+    const exist = await eventAttendance.findOne({ studentId: id, outTime: null });
+
+    if (exist) {
+      await eventAttendance.findByIdAndUpdate(
+        exist._id,
+        { outTime: Date.now() }
+      );
+    } else {
+      await eventAttendance.create({ studentId: id });
     }
-  } else {
-    return res.status(400).json({err: "User does not exist!"})
+
+    return res.json({
+      name: userData.name,
+      designation: userData.role,
+      email: userData.email,
+      phone: userData.phone,
+      branch: updatedUserData?.branch,
+      year: updatedUserData?.year,
+      roll: updatedUserData?.rollNumber,
+      dob: updatedUserData?.dob,
+      father: updatedUserData?.fatherName,
+      photo: userData.dpurl,
+      msg: (exist)?true:false},
+    );
+
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ err: "Something went wrong!" });
   }
 });
 

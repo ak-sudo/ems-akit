34f7d1da5a96aeae5507e82e8a894916e2598292
Akshat Kala <itzakshat706@gmail.com>
2026-03-12 17:30:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(botProtection);
 app.use(ipSpamProtection);
 app.use(payloadProtection);
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ems-akit.netlify.app", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
